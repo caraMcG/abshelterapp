9e4e7db064b29ee9a49898109e77e6e2a4666e6b
Cara <43040862+caraMcG@users.noreply.github.com>
2023-12-18 16:12:22 -0700

    updated save/delete functions / added comments

--- src/components/pins/Pins.jsx
+++ src/components/pins/Pins.jsx
@@ -5,48 +5,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 
-const Pins = (props) => {
+const Pins = ({setPinned, pinned }) => {
 
-    const [ pinnedList, setPinnedList] = useState([]);
     const [ recentSelect, setrecentSelect ]  = useState([]);
-    const [ recentDelete, setrecentDelete ]  = useState([]);
 
     useEffect(() => {
 
-        setrecentSelect(props.newPin);
+        console.log("useEffect fired in PinsTest")
+        setrecentSelect([...pinned]);
 
-        if(props.newPin !== recentSelect && !JSON.stringify(pinnedList).includes(JSON.stringify(props.newPin.dogName && props.newPin.dogURL))){
-            console.log("new item!");
-            setPinnedList([...pinnedList,props.newPin]);
-        }
-
-    }, [props]);
+    }, [pinned]);
     
-
-    function handlePinClick(i){
-        //unsure if want to do anything with deleted history?
-        setrecentDelete([...recentDelete, pinnedList[i.index]])
-
-        // Remove from  pinned list and update state
-        const filtered = pinnedList.filter((_,x) => x !== i.index);
-        setPinnedList(filtered);
-    }
-
 
   return (
     <>
-      { pinnedList.length > 0 ? <h3 className='pinTitle'>My Pinned Dogs ❤</h3>
-        : null}
         <div className="pinResults">
-            {pinnedList.map((item, index) => (
+            {recentSelect.map((item, index) => (
                 <div key={index} className="pinItem">
-                    
                     <div className='pinItem_topContainer'>
                     <div className='pinItem_pin'>
-                        <FontAwesomeIcon icon={faXmark} size="xl" name={index} onClick={()=> { handlePinClick({index})}}/> 
+                        <FontAwesomeIcon icon={faXmark} size="xl" name={index} onClick={()=> setPinned({index})}/> 
                     </div>
                     <div className="pinImageContainer">
-                        <img src={item.dogPic} className="pinImage" alt={'Pinned image of ' + item.dogName}/><br/>
+                        <img src={item.dogPic} className="pinImage" alt={'Pinned image of ' + item.dogName} draggable="false"/><br/>
                         </div>
                     </div> 
                     <div className="pinInfo">
@@ -55,7 +36,8 @@ const Pins = (props) => {
                         </div>
                 </div>
             ))}
-    </div>
+        </div> 
+
   </>
   )
 }
--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import './results.css'; 
 import Pins from '../pins/Pins';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbtack, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import BeatLoader from "react-spinners/BeatLoader";
 
 
 const Results = (props) => {
-  const [results, setResults] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  const [pins, setPins] = useState([]);
-  const [loaded, setLoaded] = useState(false);
+  const [results, setResults] = useState([]); //original returned results from api 
+  const [filtered, setFiltered] = useState([]); //filtered results
+  const [pinned, setPinned] = useState([]);   //Saved list of dogs
+  const [loaded, setLoaded] = useState(false); //loaded all  data once
   const lastItem = filtered.length;
+  const endpoint = props.endpoint;
 
   useEffect(() => {
     
-    const endpoint = props.endpoint;
-
     if(!loaded){
-
       (async () => {
         const data = await fetch("https://abshelterapi.onrender.com/dogs", {credentials: "omit"})
           .then(res => res.json())
@@ -30,62 +30,111 @@ const Results = (props) => {
               setFiltered(data);
             }
             else{
-              console.log("Data filtered");
+              console.log("Data filtered on load");
               setFiltered(data.filter((profile) => profile.shelterName.includes(endpoint))); 
             }
-           
-
       })()   
        
     }
     else{
       if(endpoint === 'dogs'){
-        console.log("after loading, all above");
-        setFiltered(results);
+
+        setFiltered(results.filter((profile) => !pinned.includes(profile))); 
+
       }
+      ///endpoint is not all shelters
       else{
-        setFiltered(results.filter((profile) => profile.shelterName.includes(endpoint)));
+
+        setFiltered(results.filter((profile) => profile.shelterName.includes(endpoint) && !pinned.includes(profile)));
       }
     } 
   }, [props]);
- 
-  function handlePinClick(i){
 
-        console.log("setting pin again");
-        setPins(filtered[i.index]);
-    }
+
+ //Add selected dog card to "pinned" and update filtered results for display 
+  const handleAddClick = i =>{
+
+      const newFiltered  = filtered[i.index];
+      setPinned([...pinned,newFiltered]);
+
+      //remove from main results 
+      setFiltered(filtered.filter((profile) => !profile.dogURL.includes(newFiltered.dogURL))); 
+  };
+
+
+//Remove selected dog card from "pinned" and update filtered results for display 
+  const handleDeleteClick = i =>{
+
+    //get original filtered results to find original position of profile
+    const endpointFilter = (results.filter((profile) => profile.shelterName.includes(endpoint)));
+
+    //URL of the profile just removed from pins
+    const urlToFind = pinned[i.index].dogURL;
+    const indexToInsert = endpointFilter.findIndex( profile => profile.dogURL === urlToFind);
+
+    //add back into the filtered array
+    const newFilteredArray = [...filtered];
+    newFilteredArray.splice(indexToInsert, 0, pinned[i.index])
+    setFiltered(newFilteredArray);
+
+    //remove from  pinned list
+    const newUpdatedPins = pinned.filter((_,x) => x !== i.index);
+    setPinned(newUpdatedPins);
+  };
+
 
 
   return (
-  <>
+  <>        
   <div className="dogPinned">
-    { Object.keys(pins).length !== 0  ?  < Pins newPin={pins} />
+    { Object.keys(pinned).length !== 0  ?  < Pins setPinned={handleDeleteClick} pinned={pinned}/>
       : null
     }
-   
   </div>
-    <h3 className='resultsText'>{lastItem} Results</h3><br/><br/>
-      <div className="dogResults">
-        {filtered.map((item, index) => (
-              <div key={index} className="dogItem">
-                <div className='dogItem_topContainer'>
-                  <div className='dogItem_pin'>
-                    <FontAwesomeIcon icon={faHeart} size="xl" name={index} onClick={()=> { handlePinClick({index})}}/> 
-                  </div>
-                  <div className="dogImageContainer">
-                      <img src={item.dogPic} className="dogImage" alt={'Picture of ' + item.dogName}/><br/>
+
+
+  {!loaded ? <div className='loadingAnim'>
+                    <BeatLoader
+                      color= "#455d7a"
+                      loading= {!loaded}
+                      size={30}
+                      speedMultiplier={1}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                    />
+            </div>
+    : null} 
+
+    {loaded ?  
+          <div>
+                <h3 className='resultsText'>{lastItem} Results</h3><br/><br/>
+    
+            <div className="dogResults">
+              {filtered.map((item, index) => (
+                    <div key={index} className="dogItem">
+                      <div className='dogItem_topContainer'>
+                        <div className='dogItem_pin'>
+                          <div className="dogInfo">
+                              <span>{item.dogName}</span><br/>
+                              <a href={item.dogURL} target='_blank' rel='noreferrer'>More Info</a>
+                          </div>
+                          
+                          <div className='pinHeart' >
+                            <FontAwesomeIcon icon={faHeart} size="xl" name={index} onClick={()=> { handleAddClick({index})}}/> 
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="dogImageContainer">
+                            <img src={item.dogPic} className="dogImage" alt={'Picture of ' + item.dogName} draggable="false"/><br/>
+                      </div>
                     </div>
-                 </div>
-                    
-                    <div className="dogInfo">
-                      <span>{item.dogName}</span><br/>
-                      <a href={item.dogURL} target='_blank' rel='noreferrer'>More Info</a>
-                    </div>
-              </div>
-              
-        ))}
-      
-      </div>
+                
+                ))}
+            </div>
+          </div>
+    : null}
+       
     </>
   )
 }
